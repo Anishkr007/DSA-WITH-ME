70c01f2dbbb0cf7ade864ca72a618b02effe1401
Anishkr007 <166575804+Anishkr007@users.noreply.github.com>
2026-02-04 16:03:57 +0530

    Time: 16 ms (9.95%), Space: 128.3 MB (56.12%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         int pos = 0;
         int neg=1;
-        vector<int> ans(n);
+        vector<int> ans(n,0);
 
         for(int i=0;i<n;i++){
-            if(nums[i]>=0){
+            if(nums[i]>0){
                 ans[pos]=nums[i];
                 pos+=2;
             }
