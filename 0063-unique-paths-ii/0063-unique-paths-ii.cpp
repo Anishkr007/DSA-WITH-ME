3f6b77904348930bfaf58a413f051fa63a596d9d
Anishkr007 <166575804+Anishkr007@users.noreply.github.com>
2026-02-17 00:45:22 +0530

    Time: 0 ms (100%), Space: 11.8 MB (71.55%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int f(int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>>&dp){
+    if(m<0 || n<0) return 0;
+    if(obstacleGrid[m][n]==1) return 0;
+    if(m==0 && n==0) return 1;
+    if(dp[m][n]!=-1) return dp[m][n];
+    
+    
+    int left=f(m-1,n,obstacleGrid,dp);
+    int right=f(m,n-1,obstacleGrid,dp);
+
+    return dp[m][n]=left+right;
+}
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,obstacleGrid,dp);
+    }
+};
