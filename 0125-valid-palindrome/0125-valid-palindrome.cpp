dbea2dd2abc96ac7cc8633ad201faa688b799bff
Anishkr007 <166575804+Anishkr007@users.noreply.github.com>
2026-03-06 18:56:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,9 +7,11 @@ public:
         while(left < right){
             if(!isalnum(s[left])){
                 left++;
+                continue;
             }
             else if(!isalnum(s[right])){
                 right--;
+                continue;
             }
             else{
                 if(tolower(s[left])!=tolower(s[right])){
