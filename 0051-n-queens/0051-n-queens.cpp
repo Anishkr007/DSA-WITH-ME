7a0ab893d31e4cb97439ab361f90126b851cf7aa
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-06-27 00:19:55 +0530

    Time: 0 ms (100%), Space: 10.5 MB (41.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,56 +1,38 @@
 class Solution {
 public:
-vector<vector<string>>ans;
+    vector<vector<string>> ans; 
 
-    bool ifsafe(int row,int col,vector<string> board,int n){
-        int r=row;
-        int c=col;
-
-        while(r>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-        }
-
-        r=row;
-
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-            c--;
-        }
-        r=row;
-        c=col;
-
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-            c++;
-        }
-
-        return true;
-    }
-
-    void solve(int row,vector<string>board,int n){
+    void solve(int row,vector<string>&board,vector<int>&leftrow,vector<int>&leftdia,vector<int>&rightdia,int n){
         if(row==n){
             ans.push_back(board);
             return;
         }
 
         for(int col=0;col<n;col++){
-            if(ifsafe(row,col,board,n)){
+            if(leftrow[col]==0 && leftdia[col+row]==0 && rightdia[n-1+col-row]==0){
                 board[row][col]='Q';
-                solve(row+1,board,n);
-                board[row][col]='.';
-            }
 
+                leftrow[col]=1;
+                leftdia[col+row]=1;
+                rightdia[n-1+col-row]=1;
+
+                solve(row+1,board,leftrow,leftdia,rightdia,n);
+
+                board[row][col]='.';
+                leftrow[col]=0;
+                leftdia[col+row]=0;
+                rightdia[n-1+col-row]=0;
+            }
         }
     }
-
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
+        vector<string>board(n,string(n,'.'));
 
+        vector<int>leftrow(n,0);
+        vector<int>leftdia(2*n-1,0);
+        vector<int>rightdia(2*n-1,0);
 
-        solve(0,board,n);
+        solve(0,board,leftrow,leftdia,rightdia,n);
 
         return ans;
     }
