74c576fc9885f426bbffac30a3fe796f08804a3e
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-06-29 18:29:12 +0530

    Time: 55 ms (47.18%), Space: 13.9 MB (76.3%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-bool f(int i,int j,string &p,string &s,vector<vector<int>>&dp){
-        //p=ab*cd?<-i
-        //s=abd<-j
-        if(i<0 && j<0) return true;
-        if(i<0 && j>=0) return false;
-        if(j<0 && i>=0){
-            for(int k=0;k<=i;k++){
-                if(p[k]!='*') {
-                    return false;
+    bool isMatch(string s, string p) {
+        int n = p.size();
+        int m = s.size();
+
+        vector<vector<bool>> dp(
+            n + 1,
+            vector<bool>(m + 1, false)
+        );
+
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            bool flag = true;
+
+            for (int k = 1; k <= i; k++) {
+                if (p[k - 1] != '*') {
+                    flag = false;
+                    break;
                 }
             }
-            return true;
+
+            dp[i][0] = flag;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(p[i]==s[j] || p[i]=='?'){
-            return dp[i][j]=f(i-1,j-1,p,s,dp);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (p[i - 1] == s[j - 1] ||
+                    p[i - 1] == '?') {
+
+                    dp[i][j] =
+                        dp[i - 1][j - 1];
+                }
+                else if (p[i - 1] == '*') {
+
+                    dp[i][j] =
+                        dp[i - 1][j] ||
+                        dp[i][j - 1];
+                }
+            }
         }
-        else if(p[i]=='*'){
-            return dp[i][j]=f(i-1,j,p,s,dp)||f(i,j-1,p,s,dp);
-        }
-        else return false;
 
-
-
-
-}
-    bool isMatch(string s, string p) {
-        int n=p.size();
-        int m=s.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-
-        return f(n-1,m-1,p,s,dp);
+        return dp[n][m];
     }
 };
