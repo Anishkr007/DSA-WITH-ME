9af0c32856fe2008584001f2c16429a843e8e1e7
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-06-30 13:29:17 +0530

    Time: 11 ms (48.51%), Space: 12.4 MB (39.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> last(256, -1); // ASCII characters
-        int left = 0, maxLen = 0;
+        
+        int i=0;
+        int j=0;
 
-        for (int right = 0; right < s.size(); right++) {
-            if (last[s[right]] >= left) {
-                left = last[s[right]] + 1;
+        int ans=0;
+
+        unordered_map<char,int>mp;
+
+        while(j<s.size()){
+            mp[s[j]]++;
+
+            if(mp.size()==j-i+1){
+                ans=max(ans,j-i+1);
+                j++;
+            }else if(mp.size()<j-i+1){
+                while(mp.size()<j-i+1){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0) mp.erase(s[i]);
+                    i++;
+                }
+
+                j++;
+
             }
-            last[s[right]] = right;
-            maxLen = max(maxLen, right - left + 1);
         }
-        return maxLen;
+
+        return ans;
     }
 };
