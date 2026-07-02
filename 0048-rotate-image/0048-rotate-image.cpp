21596716ef7488ee8af90313084add9315f2e626
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-07-03 00:45:35 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        
         int n=matrix.size();
 
         for(int i=0;i<n;i++){
@@ -8,9 +9,9 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+
         for(int i=0;i<n;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
-
     }
 };
