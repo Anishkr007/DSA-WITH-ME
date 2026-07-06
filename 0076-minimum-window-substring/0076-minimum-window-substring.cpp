8444772aee315360e975fd71aed9244bd8435f18
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-07-06 19:25:11 +0530

    Time: 7 ms (61.36%), Space: 11.3 MB (94.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        unordered_map<char,int>mp;
 
-        unordered_map<char,int> mp;
-
-        for(char c : t)
+        for(auto c:t){
             mp[c]++;
+        }
+        int need=t.size();
 
-        int need = t.size();
+        int l=0;
+        int r=0;
+        int mn=INT_MAX;
+        int start=0;
+        int n=s.size();
 
-        int l = 0;
-        int r = 0;
-
-        int start = 0;
-        int mn = INT_MAX;
-
-        int n = s.size();
-
-        while(r < n){
-
-            if(mp[s[r]] > 0)
+        while(r<n){
+            if(mp[s[r]]>0){
                 need--;
-
+            }
             mp[s[r]]--;
 
-            while(need == 0){
-
-                if(r-l+1 < mn){
-                    mn = r-l+1;
-                    start = l;
+            while(need==0){
+                if(r-l+1<mn){
+                    mn=r-l+1;
+                    start=l;
                 }
 
                 mp[s[l]]++;
 
-                if(mp[s[l]] > 0)
+                if(mp[s[l]]>0){
                     need++;
-
+                }
                 l++;
             }
-
             r++;
         }
 
-        if(mn == INT_MAX)
-            return "";
+        if(mn==INT_MAX) return "";
 
         return s.substr(start,mn);
     }
