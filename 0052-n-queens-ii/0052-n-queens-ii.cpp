b09765bf95c29f71e1567d3ec0c9b1070f2e56da
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-07-08 06:12:44 +0530

    Time: 1 ms (59.22%), Space: 9.3 MB (16.88%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-int count=0;
-void solve(int r,vector<string>&board, vector<int>&left, vector<int>&leftdia, vector<int>&rightdia,int n){
-    if(r==n){
-        count++;
-        return;
-
-    }
-    for(int c=0;c<n;c++){
-
-        if(left[c]==0 && leftdia[r+c]==0 && rightdia[n-1+c-r]==0){
-            board[r][c]='Q';
-            left[c]=1;
-            leftdia[c+r]=1;
-            rightdia[n-1+c-r]=1;
-
-            solve(r+1,board,left,leftdia,rightdia,n);
-
-            board[r][c]='.';
-             left[c]=0;
-            leftdia[c+r]=0;
-            rightdia[n-1+c-r]=0;
-
+    void solve(int col, vector<vector<string>>&ans,vector<string>&board,vector<int>&left,vector<int>&upper,vector<int>&lower,int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
         }
 
-    }
+        for(int row=0;row<n;row++){
+            if(left[row]==0 && upper[row+col]==0 && lower[n-1 + col - row]==0){
+                board[row][col]='Q';
+                left[row]=1;
+                upper[col+row]=1;
+                lower[n-1+col-row]=1;
 
-}
+                solve(col+1,ans,board,left,upper,lower,n);
+
+                board[row][col]='.';
+                                left[row]=0;
+                upper[col+row]=0;
+                lower[n-1+col-row]=0;
+
+
+
+            }
+        }
+    }
     int totalNQueens(int n) {
+        vector<vector<string>>ans;
         vector<string>board(n,string(n,'.'));
 
         vector<int>left(n,0);
-        vector<int>leftdia(2*n-1,0);
-        vector<int>rightdia(2*n-1,0);
+        vector<int>upper(2*n-1,0);
+        vector<int>lower(2*n-1,0);
 
-        solve(0,board,left,leftdia,rightdia,n);
+        solve(0,ans,board,left,upper,lower,n);
 
-        return count;
+        return ans.size();
     }
 };
