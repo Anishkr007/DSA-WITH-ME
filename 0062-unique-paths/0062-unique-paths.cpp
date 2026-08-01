dbccb246686743fb2761856cd5ae93125417cd05
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-08-02 00:21:26 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    
 
-    int solve(int i,int j,int m,int n){
+    int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
 
         if(i>=m || j>=n)
             return 0;
@@ -13,13 +13,13 @@ public:
         if(dp[i][j]!=-1)
             return dp[i][j];
 
-        return dp[i][j]=solve(i+1,j,m,n)+solve(i,j+1,m,n);
+        return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
     }
 
     int uniquePaths(int m, int n) {
 
-        dp.assign(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return solve(0,0,m,n);
+        return solve(0,0,m,n,dp);
     }
 };
