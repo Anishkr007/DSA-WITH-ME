d459e11344ccf50ee61f830dee0dc15bbaaff359
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-08-01 21:58:25 +0530

    Time: 2 ms (24.12%), Space: 9.2 MB (26.58%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long mod=1e9+7;
+    long long solve(int n,vector<long long>&dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n]=(2*solve(n-1,dp)+solve(n-3,dp))%mod;
+    }
+    int numTilings(int n) {
+
+        vector<long long>dp(n+1,-1);
+        long long ans=solve(n,dp);
+
+        return ans%mod;
+    }
+};
