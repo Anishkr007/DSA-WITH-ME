ff7aac8423b9c4b71362d7cd029842c4e882ec02
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-08-08 23:54:21 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.9%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,14 +5,11 @@ public:
 
         vector<vector<int>>dp(n+2,vector<int>(2,0));
 
-        //base case
-        
-
         for(int i=n-1;i>=0;i--){
-            dp[i][1]=max((-prices[i]+dp[i+1][0]),dp[i+1][1]);
-            dp[i][0]=max((prices[i]+dp[i+2][1]),dp[i+1][0]);
+            dp[i][1]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0]=max(prices[i]+dp[i+2][1],dp[i+1][0]);
         }
-    return dp[0][1];
 
+        return dp[0][1];
     }
 };
