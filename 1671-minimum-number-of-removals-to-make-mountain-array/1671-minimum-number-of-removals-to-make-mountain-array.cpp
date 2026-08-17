ef6b4446b8245fe9dbc20c32b99cc3077b4b6079
Anish kumar <166575804+Anishkr007@users.noreply.github.com>
2026-08-17 15:49:54 +0530

    Time: 50 ms (76.19%), Space: 16.3 MB (25.55%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
 
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
+        vector<int>dp1(n,1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    left[i] = max(left[i], left[j] + 1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp1[i]=max(dp1[i],dp1[j]+1);
                 }
             }
         }
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j > i; j--) {
-                if (nums[j] < nums[i]) {
-                    right[i] = max(right[i], right[j] + 1);
+        vector<int>dp2(n,1);
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[i]>nums[j]){
+                    dp2[i]=max(dp2[i],dp2[j]+1);
                 }
             }
         }
 
-        int longestMountain = 0;
+        int maxi=0;
 
-        for (int i = 0; i < n; i++) {
-            if (left[i] > 1 && right[i] > 1) {
-                longestMountain = max(
-                    longestMountain,
-                    left[i] + right[i] - 1
-                );
+        for(int i=0;i<n;i++){
+            if(dp1[i]>1 && dp2[i]>1){
+                maxi=max(maxi,dp2[i]+dp1[i]-1);
             }
         }
 
-        return n - longestMountain;
+        return n-maxi;
+
     }
 };
